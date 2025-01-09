d4a7a7fa321d3b9c91ac1b8c49afe6e21f8bf6f7
Dovlet <kdovlet777@gmail.com>
2025-01-09 16:10:28 +0300

    Adding error reporting

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 
+// Error reporting
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 echo "PHP Version ".phpversion()." is installed";
 include 'autoload.php';
